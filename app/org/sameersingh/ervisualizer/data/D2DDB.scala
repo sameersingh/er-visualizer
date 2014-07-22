a8f12f8fb63b9ddb3778564d8026ccd388318c7b
Sameer Singh <sameeersingh@gmail.com>
2014-07-21 19:16:00 -0700

    read the complete D2D database, with no pruning right now

--- app/org/sameersingh/ervisualizer/data/D2DDB.scala
+++ app/org/sameersingh/ervisualizer/data/D2DDB.scala
@@ -1,6 +1,6 @@
 package org.sameersingh.ervisualizer.data
 
-import org.sameersingh.ervisualizer.nlp.ReadProcessedDocs
+import org.sameersingh.ervisualizer.nlp.{ReadMultiROutput, ReadProcessedDocs}
 import com.typesafe.config.ConfigFactory
 import org.sameersingh.ervisualizer.freebase.MongoIO
 
@@ -9,19 +9,42 @@ import org.sameersingh.ervisualizer.freebase.MongoIO
  */
 class D2DDB {
 
+  def addRelationInfo(db: InMemoryDB) {
+    val maxProvenances = db._relationText.map({
+      case (rid, map) => map.provenances.size
+    }).max.toDouble
+    for ((rid, rt) <- db._relationText) {
+      db._relationIds += rid
+      // TODO read from freebase
+      db._relationFreebase(rid) = RelationFreebase(rid._1, rid._2, Seq.empty)
+      db._relationHeader(rid) = RelationHeader(rid._1, rid._2, rt.provenances.size.toDouble / maxProvenances)
+    }
+  }
+
   def readDB: DB = {
     // read raw documents and entity links
-    val baseDir = ConfigFactory.load().getString("nlp.data.baseDir")
+    println("Read raw docs")
+    val cfg = ConfigFactory.load()
+    val baseDir = cfg.getString("nlp.data.baseDir")
     val processedDocReader = new ReadProcessedDocs(baseDir)
     val (db, einfo) = processedDocReader.readAllDocs
 
     // fill entity info with freebase info
+    println("Read mongo info")
     val mongo = new MongoIO("localhost", 27017)
-    mongo.updateDB(db.asInstanceOf[InMemoryDB])
+    //mongo.updateDB(db.asInstanceOf[InMemoryDB])
 
-    // TODO read relations and convert that to provenances
+    // read relations and convert that to provenances
+    println("Read relations")
+    val relReader = new ReadMultiROutput(baseDir)
+    relReader.updateFromAllDocs(db.asInstanceOf[InMemoryDB])
 
-    // TODO read relation info from freebase?
+    // aggregate info to relations from provenances
+    println("Aggregate relation info")
+    addRelationInfo(db.asInstanceOf[InMemoryDB])
+
     db
   }
 }
+
+object D2DDB extends D2DDB
