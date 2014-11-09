67df1fb0287e2f2228f0e92b54b7d35652196df1
Sameer Singh <sameeersingh@gmail.com>
2014-11-09 14:30:13 -0800

    support for figer predictions in visualization, reading from mongo

--- app/org/sameersingh/ervisualizer/data/NLPReader.scala
+++ app/org/sameersingh/ervisualizer/data/NLPReader.scala
@@ -3,6 +3,7 @@ package org.sameersingh.ervisualizer.data
 import com.typesafe.config.ConfigFactory
 import nlp_serde.{Mention, Entity}
 import nlp_serde.readers.PerLineJsonReader
+import org.sameersingh.ervisualizer.freebase.MongoIO
 import org.sameersingh.ervisualizer.kba
 import play.api.libs.json.Json
 
@@ -58,10 +59,34 @@ class NLPReader {
             val map = db._docEntityProvenances.getOrElseUpdate(p.docId -> (p.sentId - 1), new mutable.HashMap)
             map(id) = map.getOrElse(id, Seq.empty) ++ Seq(p)
           }
-          /*
-          val _entityTypePredictions = new HashMap[String, Seq[String]]
-          val _entityTypeProvenances = new HashMap[String, HashMap[String, TypeModelProvenances]]
-          */
+          // Types
+          val types = new mutable.HashSet[String]()
+          val typeProvs = new mutable.HashMap[String, TypeModelProvenances]
+          for(m <- mentions) {
+            val figerStr = m.attrs.get("figer")
+            val figerPreds = figerStr.toSeq.filterNot(_.isEmpty).flatMap(str => {
+              str.split("[,\t]").map(str => {
+                val pair = str.split("@")
+                (pair(0).drop(1).replaceAll("/", ":"), pair(1).toDouble)
+              }).toSeq})
+            val startPos = d.sentences(m.sentenceId - 1).tokens(m.toks._1 - 1).chars._1 - d.sentences(m.sentenceId - 1).chars._1
+            val endPos = d.sentences(m.sentenceId - 1).tokens(m.toks._2 - 2).chars._2 - d.sentences(m.sentenceId - 1).chars._1
+            // add to local data
+            types ++= figerPreds.map(_._1)
+            for(tc <- figerPreds; t = tc._1; c = tc._2) {
+              val prov = Provenance(d.id, m.sentenceId - 1, Seq(startPos -> endPos), c)
+              typeProvs(t) = TypeModelProvenances(id, t, typeProvs.get(t).map(_.provenances).getOrElse(Seq.empty) ++ Seq(prov))
+            }
+          }
+          // add to global data
+          if(!types.isEmpty) {
+            // println(s"$id: " + types.mkString(", "))
+            db._entityTypePredictions(id) = (db._entityTypePredictions.getOrElse(id, Seq.empty) ++ types.toSeq).distinct
+          }
+          for((t,tmp)<- typeProvs) {
+            val map = db._entityTypeProvenances.getOrElseUpdate(id, new mutable.HashMap)
+            map(t) = TypeModelProvenances(id, t, map.get(t).map(_.provenances).getOrElse(Seq.empty) ++ tmp.provenances)
+          }
         }
       }
       // relations
@@ -76,12 +101,12 @@ class NLPReader {
     for (eid <- db.entityIds) {
       if (!einfo.mentions.contains(eid)) {
         println(s"Cannot find $eid in the documents..")
-        System.exit(0)
+        // System.exit(0)
       }
-      val ments = einfo.mentions(eid)
-      val name = ments.map(_.text).groupBy(x => x).map(p => p._1 -> p._2.size).maxBy(_._2)._1
-      val ner = ments.flatMap(_.ner.toSeq).groupBy(x => x).map(p => p._1 -> p._2.size).maxBy(_._2)._1
-      db._entityHeader(eid) = EntityHeader(eid, name, ner, ments.size.toDouble)
+      val ments = einfo.mentions.getOrElse(eid, Seq.empty)
+      val name = if(ments.isEmpty) "unknown" else ments.map(_.text).groupBy(x => x).map(p => p._1 -> p._2.size).maxBy(_._2)._1
+      val ner = if(ments.isEmpty) "O" else ments.flatMap(_.ner.toSeq).groupBy(x => x).map(p => p._1 -> p._2.size).maxBy(_._2)._1
+      db._entityHeader(eid) = EntityHeader(eid, name, ner, ments.size.toDouble + 1)
     }
   }
 
@@ -98,6 +123,8 @@ class NLPReader {
   }
 
   def readFreebaseInfo(db: InMemoryDB): Unit = {
+    val mongo = new MongoIO(port = 27017)
+    mongo.updateDB(db)
     /*
     val _entityInfo = new HashMap[String, EntityInfo]
     val _entityFreebase = new HashMap[String, EntityFreebase]
@@ -112,7 +139,8 @@ class NLPReader {
     val cfg = ConfigFactory.load()
     val baseDir = cfg.getString("nlp.data.baseDir") //.replaceAll(" ", "\\ ")
     readStaleness(baseDir + "/nigeria_dataset_v04.staleness.json", db)
-    readDocs(baseDir + "/nigeria_dataset_v04.nlp.lr.json.gz", db)
+    readDocs(baseDir + "/nigeria_dataset_v04.nlp.lrf.json.gz", db)
+    readFreebaseInfo(db)
     db
   }
 
